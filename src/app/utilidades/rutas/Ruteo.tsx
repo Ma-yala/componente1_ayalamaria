be4e4b78ac09d547dae1b4c7de324474b8187665
mayala11 <mayala11@cuc.edu.co>
2024-10-24 10:50:47 -0500

    que quede bien

--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Inicio } from "../../componentes/contenedor/Inicio";
+import { NoEncontrado } from "../../componentes/contenedor/NoEncontrado";
 
 export const Ruteo = () => {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
+      <Route path="*" element={<NoEncontrado />} />
     </Routes>
   );
 };
